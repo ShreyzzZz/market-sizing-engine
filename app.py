77eff3662445ca2ab56b9504072ca3937971b258
ShreyzzZz <127940336+ShreyzzZz@users.noreply.github.com>
2026-08-26 23:16:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ with st.sidebar:
         custom_base_url = None
     else:
         api_key_input = st.text_input("Groq API Key", type="password")
-        model_name = "openai/llama-3.3-70b-versatile"
+        model_name = "openai/gpt-oss-120b"
         env_var_name = "GROQ_API_KEY"
         custom_base_url = "https://api.groq.com/openai/v1"
 
